b7d3cde94647db998669b6710a4f7c52e3a3efc9
miki55555115 <miki55555115@gmail.com>
2023-04-08 11:50:41 +0200

    Add mongoose to project

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [MongooseModule],
   controllers: [AppController],
   providers: [AppService],
 })
