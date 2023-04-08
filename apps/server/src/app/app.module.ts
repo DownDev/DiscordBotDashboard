0a636a2cf745374be0c9c488b62b01a7ef99f89c
miki55555115 <miki55555115@gmail.com>
2023-04-08 16:13:21 +0200

    Add ConfigModule to imports

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [MongooseModule],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env'
+  }), MongooseModule],
   controllers: [AppController],
   providers: [AppService],
 })
